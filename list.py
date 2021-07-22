97c1c21b9ae6166fb4334c5ae8e6fdf6ecb438b6
sabaly <tedoldhead@gmail.com>
2021-07-22 10:44:26 +0800

    first

---
+++ list.py
@@ -0,0 +1,11 @@
+#這是list的練習
+a = ['audi', 'ford']#'audi'是index的0
+print(a)
+print(a[0])
+print(a[1])
+a.append('benz')
+print(a)
+print(len(a))
+'audi' in a #是非題 Ｔure or false
+print('audi' in a)
+print('lesus' in a)
